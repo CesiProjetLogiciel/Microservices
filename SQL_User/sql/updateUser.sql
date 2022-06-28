4ad9adc82799f5661e29211d3f16a0932f8edc1e
Amaury CHABANE <amaurychabane@gmail.com>
2022-06-28 15:14:51 +0200

    Fixed SQL update request

--- SQL_User/sql/updateUser.sql
+++ SQL_User/sql/updateUser.sql
@@ -2,13 +2,13 @@ SET XACT_ABORT ON;
 BEGIN TRANSACTION;
 
 UPDATE [Users]
-SET [Users].[lastname] = @Lastname,
-    [Users].[firstname] = @Firstname,
-    [Users].[pass] = @Pass,
-    [Users].[isSuspended] = @isSuspended,
-    [Users].[email] = @Email,
-    [Users].[id_UserTypes] = (SELECT [id] FROM [UserTypes] WHERE [UserTypes].[type] = @Type)
+SET [Users].[lastname] = COALESCE(@Lastname, [Users].[lastname]),
+    [Users].[firstname] = COALESCE(@Firstname, [Users].[firstname]),
+    [Users].[pass] = COALESCE(@Pass, [Users].[pass]),
+    [Users].[isSuspended] = COALESCE(@isSuspended, [Users].[isSuspended]),
+    [Users].[email] = COALESCE(@Email, [Users].[email]),
+    [Users].[id_UserTypes] = COALESCE((SELECT [UserTypes].[id] FROM [UserTypes] WHERE [UserTypes].[type] = @Type), [Users].[id_UserTypes])
 WHERE [Users].[id] = @userId
-OR [Users].[email] = @userEmail;
+   OR [Users].[email] = @userEmail;
 
 COMMIT;
